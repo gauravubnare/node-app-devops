a64233e54d0d6f3e6b3afd4a4340830d7ec423d8
Gaurav Ubnare <gauravubnare123@gmail.com>
2021-03-15 03:39:02 +0530

    Update main.tf

--- terraform-app-env/main.tf
+++ terraform-app-env/main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "allow_tls" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
